1ba642e26ef82db6ecd84d023519ccc8fa02d209
Elanis <elanis@hotmail.com>
2022-12-01 16:43:03 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use aoc2022::*;
+
+fn main() {
+    defi01::do_work();
+}
---
+++ tests/defi01.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod defi01_tests {
+    use aoc2022::defi01;
+
+    #[test]
+    fn defi01() {
+        defi01::do_work();
+    }
+}
